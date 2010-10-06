0ffbec7a3a7c12f4c13fa80cffaddc7dd20cf855
Adam <asanchez75@asanchez75-hp.(none)>
2010-10-06 15:26:29 -0500

    settings.php hidden

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -89,7 +89,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysqli://root:lachari@localhost/camino';
+$db_url = 'mysqli://root:123456@localhost/camino';
 $db_prefix = 'cam_';
 
 /**
