1b35522eb5ffd4d8a98e2867e30c4d9601073495
Adam <asanchez75@asanchez75-hp.(none)>
2010-10-13 13:56:27 -0500

    implementacion de slide con texto ovelapped

--- sites/default/themes/caminos/js/camino.js
+++ sites/default/themes/caminos/js/camino.js
@@ -1,6 +1,16 @@
 $(function($){
 	$(document).ready(function(){
+		//oculta titulo
 		$('.view-id-Experiencias_gps .view-content h3').hide();
+		//posiciona el texto del slideshow exactamente debajo de la foto principal y superpuesta
+		//solución hallada cuando me iba a almorzar a Chacarilla :)
+		altura = $('#descripcion-text').height(); 
+		var distancia; 
+		var ancho;
+		distancia = 473 - parseInt(altura); 
+		$('#descripcion-slide').css({'padding-top' : distancia});
+		ancho = $('#view-slide img').attr('width');
+		$('#descripcion-slide').css({'width' : ancho});
 	});	
 })(jQuery)
 
